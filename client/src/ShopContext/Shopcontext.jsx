83527f25307ca915940857e5581879b1bb30494f
AlaEddine <Ala.eddine@gmail.com>
2024-04-05 19:24:49 +0100

    update shopContext

--- client/src/ShopContext/Shopcontext.jsx
+++ client/src/ShopContext/Shopcontext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
-import Message from "@/component/Snackbar/Message";
-import API from "@/api/axios";
+import Message from "../component/Snackbar/Message";
+import API from '../api/axios'
 
 
 // ********
