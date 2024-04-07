aff09901b9214c1da9656cb83c0b9dd77c216d4d
AlaEddine <Ala.eddine@gmail.com>
2024-04-07 05:49:17 +0100

    another updated 65

--- server/authcontrollers/_user.js
+++ server/authcontrollers/_user.js
@@ -139,7 +139,7 @@ module.exports.GET_USER = async (req, res, next) => {
     );
     if (!result) return next(createError(401, "user not found"));
     // return res.status(200).send(result);
-    return res.status(200).json(req.cookies);
+    return res.status(200).json(req.cookies.connect.sid);
   } catch (err) {
     return next(err);
   }
