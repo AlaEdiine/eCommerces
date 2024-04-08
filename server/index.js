05f3c3c6501ef4945eaf7e9a5e5ad7af84f812d8
Ala.Eddine <143133176+AlaEdiine@users.noreply.github.com>
2024-04-08 04:26:05 +0100

    Update index.jsssss

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+})
 
 // Express-Session
 app.use(session({
