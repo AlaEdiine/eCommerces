690f1f82098da2ebdad5552bad9cba71d084c6d1
AlaEddine <Ala.eddine@gmail.com>
2024-04-22 00:53:49 +0100

    new up

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -19,8 +19,8 @@ const token = Cookies.get("Token")
 // })
 
 const API = axios.create({
-        //  baseURL : 'https://ecommerces-ncev.onrender.com' ,
-          baseURL : 'http://localhost:3001' , 
+         baseURL : 'https://ecommerces-ncev.onrender.com' ,
+          // baseURL : 'http://localhost:3001' , 
          headers:   { 
            'auth' : token
           }
